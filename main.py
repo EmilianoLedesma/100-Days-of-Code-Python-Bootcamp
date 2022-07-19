c526233262e274316f4af351a492a5167c115220
Emiliano Ledesma <71568204+EmilianoLedesma@users.noreply.github.com>
2022-07-18 20:19:27 -0500

    Band Name Generator Project
    First steps. Day 1/100.
    Uses input, variables naming and strings

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+location=input('what is the name of the city you grew up in?\n')
+pets_name = input('what is your pets name?\n')
+print("your band name could be "+ location +" "+ pets_name)
